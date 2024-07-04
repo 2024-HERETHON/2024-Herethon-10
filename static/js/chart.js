515f415293c7a4bb8f8b4171e9c51f9d2642ab1e
jeongsaeyeong <ooooo0516@naver.com>
2024-07-04 16:25:22 +0900

    202407041625

--- static/js/chart.js
+++ static/js/chart.js
@@ -5,7 +5,7 @@ new Chart(document.getElementById("doughnut-chart"), {
         datasets: [
             {
                 backgroundColor: ["var(--gray)", "var(--gray)", "var(--gray)", "var(--gray)", "#C22370"],
-                data: [10, 20, 10, 10, 50]
+                data: [1, 2, 1, 1, 5]
             }
         ]
     },
