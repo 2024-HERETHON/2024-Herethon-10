dda68e8842099c2ef4acd75811a58cbd15d726d7
jeongsaeyeong <ooooo0516@naver.com>
2024-07-04 22:50:47 +0900

    정보수정 비밀번호 삭제

--- static/js/userinfo_modify.js
+++ static/js/userinfo_modify.js
@@ -1,46 +1,28 @@
 let previousInputElement = null;
-        const userPass = document.querySelector('.putuserpass');
-        const userPassRe = document.querySelector('.putuserpassre');
+const userPass = document.querySelector('.putuserpass');
+const userPassRe = document.querySelector('.putuserpassre');
 
-        document.querySelectorAll('.modify').forEach(modifyElement => {
-            modifyElement.addEventListener('click', () => {
-                if (previousInputElement) {
-                    previousInputElement.style.border = '';
-                }
-
-                const inputElement = modifyElement.querySelector('input');
-                if (inputElement) {
-                    inputElement.style.border = '0';
-                    previousInputElement = inputElement;
-                }
-            });
-        });
-
-        function deletePass() {
-            userPass.value = '';
+document.querySelectorAll('.modify').forEach(modifyElement => {
+    modifyElement.addEventListener('click', () => {
+        if (previousInputElement) {
+            previousInputElement.style.border = '';
         }
 
-        function deletePassRe() {
-            userPassRe.value = '';
+        const inputElement = modifyElement.querySelector('input');
+        if (inputElement) {
+            inputElement.style.border = '0';
+            previousInputElement = inputElement;
         }
+    });
+});
 
-        function showPass() {
-            const currentType = userPass.getAttribute('type');
-            userPass.setAttribute('type', currentType === 'password' ? 'text' : 'password');
+document.getElementById('profileInput').addEventListener('change', function (event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            document.getElementById('profileImg').src = e.target.result;
         }
-
-        function showPassRe() {
-            const currentType = userPassRe.getAttribute('type');
-            userPassRe.setAttribute('type', currentType === 'password' ? 'text' : 'password');
-        }
-
-        document.getElementById('profileInput').addEventListener('change', function (event) {
-            const file = event.target.files[0];
-            if (file) {
-                const reader = new FileReader();
-                reader.onload = function (e) {
-                    document.getElementById('profileImg').src = e.target.result;
-                }
-                reader.readAsDataURL(file);
-            }
-        });
+        reader.readAsDataURL(file);
+    }
+});
